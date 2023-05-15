cc76526c0cfaa36b4b0000e31b70405e40f19b69
rchalamala <rchalamala@outlook.com>
2023-05-15 15:55:57 -0500

    update for fa2023

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { motion } from "framer-motion";
 const DATA_FA2022 = require("./data/IndexedTotalFA2022-23.json")
 const DATA_WI2022 = require("./data/IndexedTotalWI2022-23.json")
 const DATA_SP2023 = require("./data/IndexedTotalSP2022-23.json")
+const DATA_FA2023 = require("./data/IndexedTotalFA2023-24.json")
 
 const CURRENT_TERM = "/sp2023"
 
@@ -21,6 +22,7 @@ const courseDataSources: Record<string, CourseData> = {
   "/fa2022": DATA_FA2022,
   "/wi2022": DATA_WI2022,
   "/sp2023": DATA_SP2023,
+  "/fa2023": DATA_FA2023,
 }
 
 export const AllCourses = createContext<CourseIndex>({})
