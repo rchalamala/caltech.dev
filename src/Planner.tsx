0e830be5a95cf5679d6f295bf03e0afefeb29519
Zachary Huang <55601738+zack466@users.noreply.github.com>
2025-05-20 17:58:30 -0700

    fix weird parsing bug

--- src/Planner.tsx
+++ src/Planner.tsx
@@ -54,7 +54,11 @@ export function parseTimes(times: string): Maybe<TimeInterval>[][] {
   const ret: Maybe<TimeInterval>[][] = [[], [], [], [], []];
   const day_to_i = ["M", "T", "W", "R", "F"]; // TODO: Include Sat/Sun Courses, OM Courses
 
-  for (let line of times.split(/[,\n]/)) {
+  // super hacky fix for a parsing bug when location is A
+  let times_clean = times.replace("\nA", "");
+  console.log(times_clean)
+
+  for (let line of times_clean.split(/[,\n]/)) {
     const match = line.match(/([MTWRF]+) (\d\d):(\d\d) - (\d\d):(\d\d)/);
     if (match !== null) {
       //  An example match: [ "MWF 14:00 - 14:55", "MWF", "14", "00", "14", "55" ]
